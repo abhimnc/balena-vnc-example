2955020020321fc8df8713800c3f2c4efe8e9e06
abhimnc <abhi.iit77@gmail.com>
2024-03-14 21:41:38 +0530

    to make change using supervisor without vnc

--- vnc-app/start.sh
+++ vnc-app/start.sh
@@ -2,12 +2,22 @@
 # Xvfb :unix$DISPLAY -screen 0 1024x768x24 >& /dev/null & export DISPLAY=:unix$DISPLAY;
 
 # python3 /etc/supervisor/camera_app.py
+export XDG_RUNTIME_DIR=/tmp/runtime-root
+mkdir -p $XDG_RUNTIME_DIR
+chmod 700 $XDG_RUNTIME_DIR
 
+
+export DISPLAY=:0
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 if [ -z ${VNC_PASS} ]; then
   echo "STARTING VNC WITHOUT PASSWORD"
+  echo "STARTING X"
+  startx
   supervisord -c /etc/supervisor/supervisord_np.conf  
 else
   echo "STARTING VNC WITH PASSWORD"
+  echo "STARTING X"
+  startx
   supervisord -c /etc/supervisor/supervisord.conf
 fi
